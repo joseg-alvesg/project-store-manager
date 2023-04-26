e8a3e05509e34554989475ba8b2945d9f363f7f3
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-26 16:53:14 -0300

    create: `findById` func controller

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,10 +1,18 @@
 const { productService } = require('../services');
 
 const findAllProducts = async (req, res) => {
-  const { type, message } = await productService.findAll();
+  const { message } = await productService.findAll();
+  res.status(200).json(message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await productService.findById(+id);
+  if (type) return res.status(404).json({ message });
   res.status(200).json(message);
 };
 
 module.exports = {
   findAllProducts,
+  findById,
 };
