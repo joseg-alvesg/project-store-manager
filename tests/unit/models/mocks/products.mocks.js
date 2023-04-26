1d631ffece3d41b911a516f72e1ccb0868e96fbc
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-26 17:10:50 -0300

    create: `productMocks`

---
+++ tests/unit/models/mocks/products.mocks.js
@@ -0,0 +1,20 @@
+const allResultMock = [
+  {
+    id: 1,
+    name: "Martelo de Thor",
+  },
+  {
+    id: 2,
+    name: "Traje de encolhimento",
+  },
+];
+
+const idResultMock = {
+  "id": 1,
+  "name": "Martelo de Thor"
+}
+
+module.exports = {
+  allResultMock,
+  idResultMock,
+}
