234b1bbabd4e43eacec11ff18605ffff53e6d4ab
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 14:37:23 -0300

    implements: `findAll` with camelize

--- src/controllers/sales.contoller.js
+++ src/controllers/sales.contoller.js
@@ -2,7 +2,7 @@ const { salesService } = require('../services');
 
 const findAllSales = async (req, res) => {
   const { message } = await salesService.findAll();
-  res.status(200).json({ message });
+  res.status(200).json(message);
 };
 
 module.exports = {
--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,12 +1,13 @@
+const camelize = require('camelize');
 const conn = require('./connections');
 
 const findAll = async () => {
-  const result = conn.execute(
+  const [result] = await conn.execute(
     `SELECT * FROM StoreManager.sales AS sl
     JOIN StoreManager.sales_products AS sp
     ON sp.sale_id = sl.id`,
   );
-  return result;
+  return camelize(result);
 };
 
 module.exports = {
