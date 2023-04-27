1498648aa3d0ef39f0f37481526c91fac28ce364
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 14:46:42 -0300

    create: `findSaleId` func `controller`

--- src/controllers/sales.contoller.js
+++ src/controllers/sales.contoller.js
@@ -5,6 +5,13 @@ const findAllSales = async (_req, res) => {
   res.status(200).json(message);
 };
 
+const findSaleId = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesService.findById(+id);
+  return res.status(200).json(message);
+};
+
 module.exports = {
   findAllSales,
+  findSaleId,
 };
