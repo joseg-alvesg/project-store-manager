0b32a206e58ffa249a920f6536eed4236aa0fe51
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-26 21:22:05 -0300

    add: use json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const { productsRouter } = require('./routes');
 
 // não remova esse endpoint, é para o avaliador funcionar
