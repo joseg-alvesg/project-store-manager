b0241dcd0cfdafddec36518e5d6426825accee75
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 14:43:42 -0300

    create: `findById` func `service`

--- src/services/sales.services.js
+++ src/services/sales.services.js
@@ -5,6 +5,12 @@ const findAll = async () => {
   return { type: null, message: data };
 };
 
+const findById = async (id) => {
+  const data = await salesModel.findById(id);
+  return { type: null, message: data };
+}; 
+
 module.exports = {
   findAll,
+  findById,
 };
