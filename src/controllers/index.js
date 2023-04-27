692ecd9181b78620bb79f0fea3dfdbf53d58f8d2
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 14:23:55 -0300

    create: `findAllSales` func controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const productController = require('./products.controller');
+const salesController = require('./sales.contoller');
 
 module.exports = {
   productController,
+  salesController,
 };
---
+++ src/controllers/sales.contoller.js
@@ -0,0 +1,10 @@
+const { salesService } = require('../services');
+
+const findAllSales = async (req, res) => {
+  const { message } = await salesService.findAll();
+  res.status(200).json({ message });
+};
+
+module.exports = {
+  findAllSales,
+};
