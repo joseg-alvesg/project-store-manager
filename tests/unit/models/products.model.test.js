c19e994877c7aff17359b183b0e593042f1c2b36
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 13:13:26 -0300

    create: files

--- tests/unit/models/products.model.test.js
+++ tests/unit/models/products.model.test.js
@@ -17,7 +17,6 @@ describe("Testes de unidade do model products", function () {
   it("recuperando um produto pelo id", async function () {
     sinon.stub(connection, "execute").resolves([[idResultMock]]);
     const result = await productModel.findById(1);
-    console.log(result);
     expect(result).to.deep.equal(idResultMock);
   })
 
