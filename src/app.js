4e635bd4015fae4d5e952991b0ca938022492551
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-27 14:05:01 -0300

    create: `sales route`

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 
-const { productsRouter } = require('./routes');
+const { productsRouter, salesRouter } = require('./routes');
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -12,6 +12,8 @@ app.get('/', (_request, response) => {
 });
 
 app.use('/products', productsRouter);
+
+app.use('/sales', salesRouter);
 
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
