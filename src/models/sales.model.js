d5e435cb0f0b73131a422e78131cf16305ca3816
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-28 16:41:48 -0300

    improve: `query` delete by id create: `updateSale` implements: `query` update quantity and date

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -39,8 +39,27 @@ const insertSale = async () => {
 
 const deleteRow = async (id) => {
   const [{ affectedRows }] = await conn.execute(
-    'DELETE FROM StoreManager.sales WHERE id = ?',
+    `
+    DELETE StoreManager.sales, StoreManager.sales_products
+    FROM StoreManager.sales
+    JOIN StoreManager.sales_products ON StoreManager.sales.id = StoreManager.sales_products.sale_id
+    WHERE sales.id = ?;
+    `,
     [id],
+  );
+  return affectedRows;
+};
+
+const update = async (id, sale) => {
+  const [{ affectedRows }] = await conn.execute(
+    `
+    UPDATE StoreManager.sales_products
+    JOIN StoreManager.sales ON StoreManager.sales.id = StoreManager.sales_products.sale_id
+    SET StoreManager.sales_products.quantity = ?,
+        StoreManager.sales.date = NOW()
+    WHERE StoreManager.sales.id = ? AND StoreManager.sales_products.product_id = ?;
+    `,
+    [sale.quantity, id, sale.productId],
   );
   return affectedRows;
 };
@@ -51,4 +70,5 @@ module.exports = {
   insertProduct,
   insertSale,
   deleteRow,
+  update,
 };
