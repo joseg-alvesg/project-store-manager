e477c5b3ccc745d4b85b5ff5f67d1d6ac2295f11
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-28 12:45:07 -0300

    create: `insertSale` controller implements: validations & returns

--- src/controllers/sales.contoller.js
+++ src/controllers/sales.contoller.js
@@ -12,7 +12,19 @@ const findSaleId = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const insertSale = async (req, res) => {
+  const { body } = req;
+  const { type, message } = await salesService.insert(body);
+  if (type === 'INVALID_ID') return res.status(400).json({ message });
+  if (type === 'INVALID_QTY') return res.status(400).json({ message });
+  if (type === 'INVALID_LENGTH') return res.status(422).json({ message });
+  if (type) return res.status(404).json({ message });
+
+  return res.status(201).json(message);
+};
+
 module.exports = {
   findAllSales,
   findSaleId,
+  insertSale,
 };
