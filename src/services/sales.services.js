bc3250d94aafbec3fe184185373538f3854b1f28
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-28 12:48:13 -0300

    create: `insert` func `sertvice` implements: `validations` errors implements: `inserts` from model

--- src/services/sales.services.js
+++ src/services/sales.services.js
@@ -1,4 +1,5 @@
-const { salesModel } = require('../models');
+const { salesModel, productModel } = require('../models');
+const { validations } = require('../utils');
 
 const findAll = async () => {
   const data = await salesModel.findAll();
@@ -10,9 +11,25 @@ const findById = async (sid) => {
   if (!data || data.length < 1) return { type: 'SALE_NOT_FOUND', message: 'Sale not found' };
   const filteredData = data.map(({ id, saleId, ...rest }) => rest);
   return { type: null, message: filteredData };
-}; 
+};
+
+const insert = async (sale) => {
+  const error = await validations.validateProduct(sale);
+  if (error && error.type) return error;
+
+  const allProduct = await Promise.all(sale.map((s) => productModel.findById(s.productId)));
+  const testProductId = allProduct.every((t) => t !== undefined);
+  
+  if (!testProductId) return { type: 'PRODUCT_NOT_FOUND', message: 'Product not found' };
+
+  const id = await salesModel.insertSale();
+  await sale.map(async (e) => salesModel.insertProduct(id, e));
+  const forSale = { id, itemsSold: [...sale] };
+  return { type: null, message: forSale };
+};
 
 module.exports = {
   findAll,
   findById,
+  insert,
 };
