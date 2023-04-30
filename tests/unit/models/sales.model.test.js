94cd37bf0b71678141eb80a1574db0c93e138b01
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-30 13:33:04 -0300

    create: `findAll` sale  test

--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -1,9 +1,16 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const connection = require("../../../src/models/connections");
+const { allResultMock } = require("../services/mocks/sale.mock");
+const { salesModel } = require("../../../src/models");
+
 
 describe('Testa sales Model', function () {
-  it('retorna todos os componetes da tabela', async function () {
-    
+  it.only('retorna todos os componetes da tabela', async function () {
+    sinon.stub(connection, 'execute').resolves([allResultMock])
+
+    const result = await salesModel.findAll();
+
+    expect(result).to.deep.equal(allResultMock)
   })
 })
---
+++ tests/unit/services/mocks/sale.mock.js
@@ -0,0 +1,29 @@
+const allResultMock = [
+  {
+    id: 1,
+    date: '2023-04-30T16:21:06.000Z',
+    saleId: 1,
+    productId: 1,
+    quantity: 5
+  },
+  {
+    id: 1,
+    date: '2023-04-30T16:21:06.000Z',
+    saleId: 1,
+    productId: 2,
+    quantity: 10
+  },
+  {
+    id: 2,
+    date: '2023-04-30T16:21:06.000Z',
+    saleId: 2,
+    productId: 3,
+    quantity: 15
+  }
+]
+
+
+
+module.exports = {
+  allResultMock,
+}
