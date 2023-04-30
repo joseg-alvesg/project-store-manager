48b423949b0edcf3058b68ed7147222d128b0018
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-30 13:43:20 -0300

    coverage: `findById` sale model test

--- tests/unit/models/sales.model.test.js
+++ tests/unit/models/sales.model.test.js
@@ -1,16 +1,26 @@
 const { expect } = require("chai");
 const sinon = require("sinon");
 const connection = require("../../../src/models/connections");
-const { allResultMock } = require("../services/mocks/sale.mock");
+const { allResultMock, resultByIdMock } = require("../services/mocks/sale.mock");
 const { salesModel } = require("../../../src/models");
 
 
 describe('Testa sales Model', function () {
-  it.only('retorna todos os componetes da tabela', async function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  it('retorna todos os componetes da tabela', async function () {
     sinon.stub(connection, 'execute').resolves([allResultMock])
 
     const result = await salesModel.findAll();
 
     expect(result).to.deep.equal(allResultMock)
+  })
+
+  it('testa a busca pelo id', async function () {
+    sinon.stub(connection, 'execute').resolves([resultByIdMock])
+    const result = await salesModel.findById(1)
+    expect(result).to.deep.equal(resultByIdMock)
   })
 })
--- tests/unit/services/mocks/sale.mock.js
+++ tests/unit/services/mocks/sale.mock.js
@@ -23,7 +23,21 @@ const allResultMock = [
 ]
 
 
+const resultByIdMock = [
+  {
+    date: "2023-04-30T16:21:06.000Z",
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    date: "2023-04-30T16:21:06.000Z",
+    productId: 2,
+    quantity: 10,
+  },
+];
+
 
 module.exports = {
   allResultMock,
+  resultByIdMock,
 }
