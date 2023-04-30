142b2d2b411b40fe6a65495d92ed7277cf5e2d28
joseg-alvesg <cinhoalves85@gmail.com>
2023-04-30 13:24:52 -0300

    estructure: `sales` test

---
+++ tests/unit/models/sales.model.test.js
@@ -0,0 +1,9 @@
+const { expect } = require("chai");
+const sinon = require("sinon");
+const connection = require("../../../src/models/connections");
+
+describe('Testa sales Model', function () {
+  it('retorna todos os componetes da tabela', async function () {
+    
+  })
+})
