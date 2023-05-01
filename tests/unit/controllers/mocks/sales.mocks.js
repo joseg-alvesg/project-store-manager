5b89cc3947ac2bf4670764ba46f342d1570c87ed
joseg-alvesg <cinhoalves85@gmail.com>
2023-05-01 17:09:47 -0300

    implements: `100% coverage`

--- tests/unit/controllers/mocks/sales.mocks.js
+++ tests/unit/controllers/mocks/sales.mocks.js
@@ -23,19 +23,41 @@ const allSalesMock = [
 ];
 
 const saleByIdResultMock = [
-  {
-    date: "2023-05-01T18:45:24.000Z",
-    productId: 1,
-    quantity: 5,
-  },
-  {
-    date: "2023-05-01T18:45:24.000Z",
-    productId: 2,
-    quantity: 10,
-  },
+  { date: "2023-05-01T18:45:24.000Z", productId: 1, quantity: 5 },
+  { date: "2023-05-01T18:45:24.000Z", productId: 2, quantity: 10 },
 ];
+
+const insertResolveMock = {
+  id: 3,
+  itemsSold: [
+    { productId: 1, quantity: 1 },
+    { productId: 2, quantity: 5 },
+  ],
+};
+
+const insertCorrectSaleBodyMock = [
+  { productId: 1, quantity: 1 },
+  { productId: 2, quantity: 5 },
+];
+
+const updateCorrectSaleBodyMock = [
+  { productId: 1, quantity: 1 },
+  { productId: 2, quantity: 5 },
+];
+
+const updatedMock = {
+  saleId: 1,
+  itemsUpdated: [
+    { productId: 1, quantity: 50 },
+    { productId: 2, quantity: 199 },
+  ],
+};
 
 module.exports = {
   allSalesMock,
   saleByIdResultMock,
-}
+  insertCorrectSaleBodyMock,
+  insertResolveMock,
+  updateCorrectSaleBodyMock,
+  updatedMock,
+};
--- tests/unit/controllers/sales.controllers.test.js
+++ tests/unit/controllers/sales.controllers.test.js
@@ -4,7 +4,7 @@ const sinon = require("sinon");
 const sinonChai = require("sinon-chai");
 const { salesService } = require("../../../src/services");
 const { salesController } = require("../../../src/controllers");
-const { allSalesMock, saleByIdResultMock } = require("./mocks/sales.mocks");
+const { allSalesMock, saleByIdResultMock, insertCorrectSaleBodyMock, insertResolveMock, insertWrongSaleBodyMock1, insertWrongSaleBodyMock2, insertWrongSaleBodyMock3, updateCorrectSaleBodyMock, updatedMock } = require("./mocks/sales.mocks");
 
 
 chai.use(sinonChai);
@@ -58,4 +58,135 @@ describe("Testes de unidade do controller sales", function () {
     expect(res.status).to.have.been.calledWith(404);
     expect(res.json).to.have.been.calledWith({message: 'Sale not found'});
   });
+
+  it('Insere uma nova venda com sucesso', async function () {
+    const res = {};
+    const req = { body: insertCorrectSaleBodyMock };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(salesService, 'insert').resolves({type: null, message: insertResolveMock})
+
+    await salesController.insertSale(req, res)
+    
+    expect(res.status).to.have.been.calledWith(201)
+    expect(res.json).to.have.been.calledWith(insertResolveMock)
+  })
+
+  it('insere valores errados no insert', async function () {
+    const res = {};
+    const req = { body: insertWrongSaleBodyMock1 };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon.stub(salesService, 'insert')
+      .onCall(0).resolves({ type: 'INVALID_ID', message: '"productId" is required' })
+      .onCall(1).resolves({ type: 'INVALID_QTY', message: '"quantity" is required' })
+      .onCall(2).resolves({ type: 'INVALID_LENGTH', message: '"quantity" must be greater than or equal to 1' })
+      .onCall(3).resolves({ type: 'PRODUCT_NOT_FOUND', message: 'Product not found' })
+
+    await salesController.insertSale(req, res)
+    expect(res.status).to.have.been.calledWith(400)
+    expect(res.json).to.have.been.calledWith({message: '"productId" is required'});
+
+    await salesController.insertSale(req, res);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({message: '"quantity" is required'});
+
+    await salesController.insertSale(req, res);
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been.calledWith({message: '"quantity" must be greater than or equal to 1'});
+
+    await salesController.insertSale(req, res);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({message: "Product not found"});
+  })
+
+  it('Remove uma venda com sucesso', async function () {
+    const res = {};
+    const req = { params: {id: 1} };
+    res.sendStatus = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, 'deleteRow')
+      .resolves({ type: null, message: '' });
+
+    await salesController.deleteSale(req, res);
+
+    expect(res.sendStatus).to.have.been.calledWith(204);
+  })
+
+  it('Remove uma venda com id invalido', async function () {
+    const res = {};
+    const req = { params: { id: 99999 } };
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, "deleteRow")
+      .resolves({ type: 'SALE_NOT_FOUND', message: "Sale not found" });
+
+    await salesController.deleteSale(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);  
+    expect(res.json).to.have.been.calledWith({message: 'Sale not found'});
+  })
+
+  it('Atualiza uma venda com sucesso', async function () {
+      const res = {};
+    const req = { params: { id: 1 }, body: updateCorrectSaleBodyMock };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      sinon
+        .stub(salesService, 'update')
+        .resolves({ type: null, message: updatedMock });
+
+      await salesController.updateSale(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(updatedMock);
+  })
+
+  it('testa os erros durante atualização de uma venda', async function () {
+    const res = {};
+    const req = { params: { id: 1 }, body: updateCorrectSaleBodyMock };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+
+    sinon
+      .stub(salesService, "update")
+      .onCall(0).resolves({ type: "INVALID_ID", message: '"productId" is required' })
+      .onCall(1).resolves({ type: "INVALID_QTY", message: '"quantity" is required' })
+      .onCall(2).resolves({
+        type: "INVALID_LENGTH",
+        message: '"quantity" must be greater than or equal to 1',
+      })
+      .onCall(3).resolves({ type: "PRODUCT_NOT_FOUND", message: "Product not found" });
+
+    await salesController.updateSale(req, res);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"productId" is required',
+    });
+
+    await salesController.updateSale(req, res);
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({
+      message: '"quantity" is required',
+    });
+
+    await salesController.updateSale(req, res);
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been.calledWith({
+      message: '"quantity" must be greater than or equal to 1',
+    });
+
+    await salesController.updateSale(req, res);
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: "Product not found" });
+  })
 });
